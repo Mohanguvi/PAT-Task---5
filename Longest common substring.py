5d1e5d80f549b8a61a9f2d8c811c0e0442446695
Mohan raj <147843991+Mohanguvi@users.noreply.github.com>
2023-11-25 01:40:58 +0530

    Longest common substring.py

--- Longest common substring.py
+++ Longest common substring.py
@@ -1,22 +1,17 @@
-# Two strings
+# Get the two strings from the user input
 str1 = input("Enter the string 1: ")
 str2 = input("Enter the string 2: ")
 
-# Generate all substrings for each string
+# Set operation used to find all possible ways to get the longest common sub-strings between two strings
 sub_str1 = {str1[i:j] for i in range(len(str1)) for j in range(i + 1, len(str1) + 1)}
 sub_str2 = {str2[i:j] for i in range(len(str2)) for j in range(i + 1, len(str2) + 1)}
 
-# Find the common substrings using set intersection
+# Using intersection to check the longest common sub-string in the two strings
 substrings = sub_str1 & sub_str2
 
-# Initialize a counter for common substring occurrences
-count = 0
 
-# Count the occurrences of common substrings in both strings
-for common_substring in substrings:
-    count1 = str1.count(common_substring)
-    count2 = str2.count(common_substring)
-    count += min(count1, count2)
+# Find the longest common substring value 
+longest_common = max(len(substring) for substring in substrings) if substrings else 0
 
-# Output the result
-print("Common substrings count:", count)
+# Longest common sub-string value will be displayed 
+print("Length of Longest Common Substring:", longest_common)
